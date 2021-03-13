25ff26a8cba34b77ca843169bc823bf6e9bcd59c
Richard <richard.yang.tong@gmail.com>
2021-03-13 15:45:56 -0500

    Disabling api routes to see if it fixes Heroku problems.

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ mongoose.connect(
 });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
