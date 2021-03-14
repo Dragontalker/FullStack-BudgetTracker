27e7f8e437879aab4b9d9ecaa14435a86f52d84c
Richard <richard.yang.tong@gmail.com>
2021-03-14 18:44:52 -0400

    Added catch back responses for resigter().

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,5 +1,11 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw-register.js')
+        navigator.serviceWorker.register('sw-register.js', { scope: '/' })
+            .then((registration) => {
+                console.log(registration.scope);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     });
 };
