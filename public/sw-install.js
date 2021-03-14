12ad1f09e42237d04f02c17dd4979374c076804f
Richard <richard.yang.tong@gmail.com>
2021-03-14 18:50:46 -0400

    Added the list of files we want to cashe.

--- public/sw-install.js
+++ public/sw-install.js
@@ -1,3 +1,11 @@
 this.addEventListener('install', (event) => {
     cashes.open('my-cashe-v1')
+        .then((cashe) => {
+            cashe.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/index.js'
+            ]);
+        })
 });
