ef0ae0d23d91a4769575256fa24034ae23229ef6
Richard <richard.yang.tong@gmail.com>
2021-03-15 21:24:23 -0400

    Defined different properties for cache.

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,11 @@
+let CACHE_VERSION = 0;
+let CACHE_NAME = 'cache_v' + CACHE_NAME;
+let CACHE_URLS = [
+    '/',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
 console.log('Hello service world!');
 
 self.addEventListener('fetch', (event) => {
