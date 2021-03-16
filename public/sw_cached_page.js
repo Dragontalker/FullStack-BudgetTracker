6867063ab49c3ea8b2a75c353a76493f3faf364a
Richard <richard.yang.tong@gmail.com>
2021-03-16 06:56:19 -0400

    Updated the installation event handle so that it now caches the static file as we reload.

--- public/sw_cached_page.js
+++ public/sw_cached_page.js
@@ -2,13 +2,23 @@ const cacheName = 'v1';
 
 const cacheAssets = [
     'index.html',
-    'style.css',
+    'styles.css',
     'index.js'
 ];
 
 // Call Install Event
 self.addEventListener('install', e => {
     console.log("Service Worker: Installed");
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log("Service Worker: Caching Files");
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
 // Call Activate Event
