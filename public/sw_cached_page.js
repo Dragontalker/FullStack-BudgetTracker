8bbed550bbc203b182c430470951d443cd3814a2
Richard <richard.yang.tong@gmail.com>
2021-03-16 06:45:32 -0400

    Added the name of the cache and assets we want to cache.

--- public/sw_cached_page.js
+++ public/sw_cached_page.js
@@ -1,3 +1,11 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    'index.html',
+    'style.css',
+    'index.js'
+];
+
 // Call Install Event
 self.addEventListener('install', e => {
     console.log("Service Worker: Installed");
