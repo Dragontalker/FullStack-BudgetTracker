7a696bf3eebfbf569d98b23018b3e63531ff7ca2
Richard <richard.yang.tong@gmail.com>
2021-03-16 14:49:59 -0400

    Switched out service worker v2 and replaced with v1 to eliminate the console error.

--- public/index.js
+++ public/index.js
@@ -226,7 +226,7 @@ document.querySelector("#sub-btn").onclick = function() {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./sw_cached_site.js')
+      .register('./sw_cached_page.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
--- public/sw_cached_page.js
+++ public/sw_cached_page.js
@@ -1,8 +1,26 @@
-const cacheName = 'v2';
+const cacheName = 'v1';
+
+const cacheAssets = [
+    'index.html',
+    'styles.css',
+    'index.js',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
 
 // Call Install Event
 self.addEventListener('install', e => {
     console.log("Service Worker: Installed");
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log("Service Worker: Caching Files");
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
 // Call Activate Event
@@ -28,17 +46,6 @@ self.addEventListener('fetch', e => {
     console.log("Service Worker: Fetching");
     e.respondWith(
         fetch(e.request)
-            .then(res => {
-                // Make copy/clone of response
-                const resClone = res.clone();
-                // Open cache
-                caches
-                    .open(cacheName)
-                    .then(cache => {
-                        // Add response to cache
-                        cache.put(e.request, resClone);
-                    });
-                return res;
-            }).catch(err => caches.match(e.request).then(res => res))
-    );
+            .catch(() => caches.match(e.request))
+    )
 });
