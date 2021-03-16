efc894c00b1483898a8b55474900d83ed64649e8
Richard <richard.yang.tong@gmail.com>
2021-03-16 14:55:14 -0400

    Removed test-purpose console.log.

--- public/sw_cached_page.js
+++ public/sw_cached_page.js
@@ -10,13 +10,10 @@ const cacheAssets = [
 
 // Call Install Event
 self.addEventListener('install', e => {
-    console.log("Service Worker: Installed");
-
     e.waitUntil(
         caches
             .open(cacheName)
             .then(cache => {
-                console.log("Service Worker: Caching Files");
                 cache.addAll(cacheAssets);
             })
             .then(() => self.skipWaiting())
