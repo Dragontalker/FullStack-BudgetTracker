dc2e920137bb66b66021fea49ae19be46b2818c1
Richard <richard.yang.tong@gmail.com>
2021-03-16 09:13:18 -0400

    Finished replacement of service worker and ready to test on Heroku.

--- public/index.js
+++ public/index.js
@@ -156,7 +156,7 @@ document.querySelector("#sub-btn").onclick = function() {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./sw_cached_page.js')
+      .register('./sw_cached_site.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
--- public/sw_cached_site.js
+++ public/sw_cached_site.js
@@ -28,6 +28,17 @@ self.addEventListener('fetch', e => {
     console.log("Service Worker: Fetching");
     e.respondWith(
         fetch(e.request)
-            .catch(() => caches.match(e.request))
-    )
+            .then(res => {
+                // Make copy/clone of response
+                const resClone = res.clone();
+                // Open cache
+                caches
+                    .open(cacheName)
+                    .then(cache => {
+                        // Add response to cache
+                        cache.put(e.request, resClone);
+                    });
+                return res;
+            }).catch(err => caches.match(e.request).then(res => res))
+    );
 });
